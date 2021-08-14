7ba742830a6dff52d129ed362724fffab3f36da5
Dany-Rolon <dany_rolon@outlook.com>
2021-08-14 09:51:44 -0300

    react-native-stack-navigation and depencies installed

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import 'react-native-gesture-handler';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hola Mundo</Text>
+    </View>
+  )
+}
+
+export default App
